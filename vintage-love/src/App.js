27c8677b6ef3b00a56f626f28cf897de790d67ce
David Downie <daviddownie@gmail.com>
2020-09-06 19:30:33 -0400

    basic setup

---
+++ vintage-love/src/App.js
@@ -0,0 +1,195 @@
+import React, { Component, Fragment } from "react";
+import "./App.scss";
+import lady from "./assets/lady.jpg";
+import logo from "./assets/logo.jpg";
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <div className="header__logoContainer">
+          <img src={logo} alt="Vintage Love Logo" />
+        </div>
+      </header>
+    );
+  }
+}
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <div className="navbar__wrapper">
+          <div className="navbar__vlInfo">
+            <h1>Vintagelove</h1>
+          </div>
+          <ul className="navbar__burger">
+            <li class="navbar__line1"></li>
+            <li class="navbar__line2"></li>
+            <li class="navbar__line3"></li>
+          </ul>
+          <ul className="navbar__nav">
+            <li>
+              <p>
+                <a href="#who">Who</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="#what">What</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="#where">Where</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="#contact">Contact</a>
+              </p>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+class Welcome extends Component {
+  render() {
+    return (
+      <section className="welome">
+        <h2>Who</h2>
+        <div className="welcome__imageContainer">
+          <img src="" alt="Pin-up girl" />
+        </div>
+      </section>
+    );
+  }
+}
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="card__imageContainer">
+          <img src="" alt="switchplate" />
+          <div class="galleryHover">
+            <div class="imageInfoContainer">
+              <div class="imageInfo">
+                <h3>switchplate name</h3>
+                <p>switchplate description</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Gallery extends Component {
+  render() {
+    return (
+      <section className="gallery">
+        <h2>Products</h2>
+        <Card />
+      </section>
+    );
+  }
+}
+
+class Stores extends Component {
+  render() {
+    return (
+      <div className="store">
+        <div className="store__info">
+          <a href="">
+            <p>Black Star Studios</p>
+          </a>
+          <p>Invermere</p>
+          <p>British Columbia</p>
+          <p>1021 7th Avenue</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Purchase extends Component {
+  render() {
+    return (
+      <section className="purchase">
+        <div className="purchase__wrapper">
+          <h2 className="purchase__title">Where</h2>
+        </div>
+        <div className="purchase__etsy">
+          <a href="">
+            <p>Etsy</p>
+          </a>
+        </div>
+      </section>
+    );
+  }
+}
+
+class Form extends Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" />
+        <label htmlFor="message">message</label>
+        <textarea type="text" name="message" id="message" />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+class Contact extends Component {
+  render() {
+    return (
+      <section className="contact">
+        <h2>Contact</h2>
+        <Form />
+      </section>
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <div className="footer__ownership">
+          <p className="footer__copy">copyright &copy; Adrianne Kulling 2020</p>
+          <p className="footer__built">
+            Site build by <a href="http://davidjdownie.ca/">David Downie</a>
+          </p>
+        </div>
+      </footer>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <Navbar />
+        <Welcome />
+        <Gallery />
+        <Purchase />
+        <Contact />
+        <Footer />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
