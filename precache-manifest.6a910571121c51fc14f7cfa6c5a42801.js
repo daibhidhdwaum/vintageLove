2ecb4f7492dcc5668d18a0eee498ccc6d33a7b4f
David Downie <daviddownie@gmail.com>
2020-10-03 14:52:55 -0400

    Updates

---
+++ precache-manifest.6a910571121c51fc14f7cfa6c5a42801.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e49ec7b97cf87594dd6ffb6af94c93d",
+    "url": "/vintageLoveReact/index.html"
+  },
+  {
+    "revision": "eb1ca1241558222347c8",
+    "url": "/vintageLoveReact/static/css/main.6213e845.chunk.css"
+  },
+  {
+    "revision": "4d9fffe63135c34985c1",
+    "url": "/vintageLoveReact/static/js/2.5b88443d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vintageLoveReact/static/js/2.5b88443d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb1ca1241558222347c8",
+    "url": "/vintageLoveReact/static/js/main.fe1629ec.chunk.js"
+  },
+  {
+    "revision": "229efdec52f38ab93a59",
+    "url": "/vintageLoveReact/static/js/runtime-main.ffc817af.js"
+  },
+  {
+    "revision": "35ff2b8a7bbd4136c0bfc5aca4e541c3",
+    "url": "/vintageLoveReact/static/media/bubble.35ff2b8a.png"
+  },
+  {
+    "revision": "a2e7aafa438be97b2b2517e42f683270",
+    "url": "/vintageLoveReact/static/media/logo.a2e7aafa.png"
+  },
+  {
+    "revision": "d776db1b0bbb7c89c7d13cfe5e5587d9",
+    "url": "/vintageLoveReact/static/media/pinupRecline.d776db1b.png"
+  }
+]);
